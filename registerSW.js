5bb558ae58a9d00c77faae573c208857efa1b9b7
Ömer Can Karadağ <krdg.omercan@hotmail.com>
2025-03-03 20:26:24 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/cli-portfolio/sw.js', { scope: '/cli-portfolio/' })})}
